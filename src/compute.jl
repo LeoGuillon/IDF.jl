41a53098f1caf34a8edea32ffae42c9ab5a2c943
LeoGuillon <leo.guillon@polytechnique.edu>
2021-05-10 16:46:35 -0400

    Refined BIC_GEV function

--- src/compute.jl
+++ src/compute.jl
@@ -6,6 +6,7 @@ Compute the BIC of a given vector `Y` under the model of a GEV.
 function BIC_GEV(Y::Vector{Float64})
     p = gevfit(y).θ̂
     n = length(Y)
+    k = length(p)
 
-    return 3*log(n) - 2log(logL(Y, p[1], p[2], p[3]))
+    return k*log(n) - 2loglikelihood(GeneralizedExtremeValue(p[1], exp(p[2]), p[3]), y)
 end
