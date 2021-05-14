45eb2718bf3352ad7b67720637cd10782dfb362f
LeoGuillon <leo.guillon@polytechnique.edu>
2021-05-14 09:19:12 -0400

    Added logL function

--- src/compute.jl
+++ src/compute.jl
@@ -1,4 +1,14 @@
 """
+    logL
+
+Compute the log-likelihood of a given vector `Y` under a GEV of parameters μ, σ and ξ
+"""
+function logL(Y::Vector{Float64}, μ::Float64, σ::Float64, ξ::Float64)
+    G = GeneralizedExtremeValue(μ, σ, ξ)
+    return loglikelihood(G, Y)
+end
+
+"""
     BIC_GEV
 
 Compute the BIC of a given vector `Y` under the model of a GEV.
@@ -8,5 +18,5 @@ function BIC_GEV(Y::Vector{Float64})
     n = length(Y)
     k = length(p)
 
-    return k*log(n) - 2loglikelihood(GeneralizedExtremeValue(p[1], exp(p[2]), p[3]), y)
+    return k*log(n) - 2logL(Y, p[1], exp(p[2]), p[3])
 end
